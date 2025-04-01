8d9a4a7047d5cfa907ff0da2d81b4def567ad057
Toru1001 <jmirafuentes47@gmail.com>
2025-04-02 01:48:21 +0800

    Added log-in UI and Supabase client

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+"use client";
+import { useEffect } from "react";
 import PageHeader from "./components/header/pageHeader";
 import Hero from "./components/landing/hero";
 import Banner from "./components/landing/banner";
@@ -8,19 +9,20 @@ import ProductSection from "./components/landing/product";
 import OurShop from "./components/landing/ourShop";
 import AboutUs from "./components/landing/aboutUs";
 import Footer from "./components/landing/footer";
+import { supabase } from "./lib/supabase";
 
 export default function Home() {
   return (
     <>
-    <PageHeader/>
-    <Hero/>
-    <Banner/>
-    <Section2/>
-    <Banner2/> 
-    <ProductSection/>
-    <OurShop/>
-    <AboutUs/>
-    <Footer/>
+      <PageHeader />
+      <Hero />
+      <Banner />
+      <Section2 />
+      <Banner2 />
+      <ProductSection />
+      <OurShop />
+      <AboutUs />
+      <Footer />
     </>
   );
 }
