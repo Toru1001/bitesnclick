43ef879b9bed8dea7d0eede5b12456459f76a067
annayanna123 <annamaybbote@gmail.com>
2025-04-08 00:16:28 +0800

    create discounts dummy

---
+++ app/discounts/page.tsx
@@ -0,0 +1,10 @@
+import CreateDiscountForm from "./create_discounts";
+
+export default function Home() {
+    return (
+      <div className="max-w-lg mx-auto mt-10">
+      <CreateDiscountForm />
+    </div>
+    );
+  }
+  
