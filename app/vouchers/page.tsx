a15aa6651b914d60b56fd25bbe52db5eebacdb6a
annayanna123 <annamaybbote@gmail.com>
2025-04-13 20:22:47 +0800

    Save local changes

--- app/vouchers/page.tsx
+++ app/vouchers/page.tsx
@@ -1,11 +1,13 @@
 import CreateVoucher from "./create_vouchers";
+import ViewVouchers from "./view_vouchers";
 
-
-export default function Home() {
-    return (
-      <div className="max-w-lg mx-auto mt-10">
+export default function ViewVouchersPage() {
+  return (
+    <div className="max-w-4xl mx-auto mt-10">
       <CreateVoucher />
+      <ViewVouchers />
     </div>
-    );
-  }
+  );
+}
+
   
